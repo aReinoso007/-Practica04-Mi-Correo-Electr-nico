c4d94be167aac66c34ae4986084bce860dda1bd4
Alex Reinoso <ralexjessiel@gmail.com>
2019-04-25 15:28:20 -0500

    Update crear_usuario.php

--- public/controladores/crear_usuario.php
+++ public/controladores/crear_usuario.php
@@ -2,4 +2,14 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<style type=
+<style type="text/css" rel="stylesheet">
+    .error{
+        color:red;
+    }
+</style>
+</head>
+<body>
+
+<?php
+    include '../config/conexionDB.php';
+    $cedula=isset($_POST["cedula"])? mb_strtoupper($_POST["nombres"])
