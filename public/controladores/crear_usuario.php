58bab812cf2cb8989d6ef4d9cf8d3a0239887f0c
Alex Reinoso <ralexjessiel@gmail.com>
2019-04-29 08:30:18 -0500

    Login
    falta agregar ultima parte de login

--- public/controladores/crear_usuario.php
+++ public/controladores/crear_usuario.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 <meta charset="UTF-8">
+<title>Crear Nuevo Usuario</title>
 <style type="text/css" rel="stylesheet">
     .error{
         color:red;
@@ -9,7 +10,36 @@
 </style>
 </head>
 <body>
+    <?php
 
-<?php
-    include '../config/conexionDB.php';
-    $cedula=isset($_POST["cedula"])? mb_strtoupper($_POST["nombres"])
+        include '../../config/conexionBD.php';
+
+            $cedula     = isset($_POST["cedula"]) ? trim($_POST["cedula"]):null;
+            $nombres    = isset($_POST["nombres"]) ? mb_strtoupper(trim($_POST["nombres"]), 'UTF-8'):null;
+            $apellidos  = isset($_POST["apellidos"]) ? mb_strtoupper(trim($_POST["apellidos"]), 'UTF-8'):null;
+            $direccion  = isset($_POST["direccion"]) ? mb_strtoupper(trim($_POST["direccion"]), 'UTF-8'):null;
+            $telefono   = isset($_POST["telefono"]) ? trim($_POST["telefono"]):null;
+            $correo     = isset($_POST["correo"]) ? trim($_POST["correo"]):null;
+            $contrasena = isset($_POST["contrasena"]) ? trim($_POST["contrasena"]):null;
+            $fechaNac   = isset($_POST["fechaNac"]) ? trim($_POST["fechaNac"]):null;
+    
+
+            $sql = "INSERT INTO usuario VALUES(0, '$cedula', '$nombres', '$apellidos', '$direccion', 
+            '$telefono', '$correo', MD5('$contrasena'), '$fechaNac', 'N', null, null)";
+
+            if($conn->query($sql) === TRUE){
+                echo "<p> Se ha creado los datos personales correctamente</p>";
+            }else{
+                if($conn->errno==1062){
+                    echo "<p class='error'>La persona con la cedula $cedula ya esta registrada en el sistema</p>";
+                }else{
+                    echo "<p class='error'>Error:" .mysqli_error($conn)."</p>";
+                }
+            }
+
+        $conn->close();
+            echo "<a href= '../vista/crear_usuario.html'>Regresar</a>";
+    ?>
+
+</body>
+</html>
---
+++ public/controladores/login.php
@@ -0,0 +1,22 @@
+<?php
+
+session_start();
+
+include '../../config/conexionBD.php';
+
+$usuario =isset($_POST["correo"]) ? trim($_POST["correo"]):null;
+$contrasena = isset($_POST["contrasena"]) ? trim($_POST["contrasena"]):null;
+
+$sql = "SELECT * FROM usuario WHERE usu_correo = '$usuario' and usu_password=MD5('$contrasena')";
+
+$result = $conn->query($sql);
+if($result->num_rows>0){
+    $_SESSION['isLogged']=TRUE;
+    header("Location:../../admin/vista/usuario/index.php");
+}else{
+    header("Location: ../vista/login.html");
+}
+
+$conn->close();
+
+?>
